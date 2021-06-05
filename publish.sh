7822e48fdbc6cf6bb7171c7ca02a93adc3c6bc34
Jim Pick <jim@jimpick.com>
2021-06-04 18:19:26 -0700

    Change publish output schema for miner power

--- publish.sh
+++ publish.sh
@@ -12,5 +12,5 @@ COUNT=0
   echo $((++COUNT)) $MINER 1>&2
   # {"window":{"start":"2021-05-30T00:00:00.000Z","end":"2021-05-31T00:00:00.000Z"},"avg(rawBytePower)":0.0,"avg(qualityAdjPower)":0.0}
   cat $(ls $m/*.json) | head -1 | jq "{ miner: \"$MINER\", rawBytePower: .[\"avg(rawBytePower)\"], qualityAdjPower: .[\"avg(qualityAdjPower)\"] }"
-done) | jq -s "{ date: \"$DATE\", records: . }" > dist/miner-power-daily-average-latest/miner-power-daily-average-latest.json
+done) | jq -s "{ date: \"$DATE\", miners: map({ key: .miner, value: { qualityAdjPower: .qualityAdjPower, rawBytePower: .rawBytePower } }) | from_entries }" > dist/miner-power-daily-average-latest/miner-power-daily-average-latest.json
 (cd dist/miner-power-daily-average-latest; head miner-power-daily-average-latest.json; hub bucket push)
