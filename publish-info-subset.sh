479c60cd72356e11ab979a5180a209450e8f6443
Jim Pick <jim@jimpick.com>
2021-06-15 14:25:01 -0700

    Change miner info subset format

--- publish-info-subset.sh
+++ publish-info-subset.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
 IFS="$(printf '\n\t')"
+DATE=$(node -e 'console.log((new Date()).toISOString())')
 
 # Latest multiday power average
 mkdir -p dist/miner-info-subset-latest
 if [ -f output/miner_info/json_latest_subset/_SUCCESS ] ; then
   PART=$(ls output/miner_info/json_latest_subset/part*.json | head -1)
-  cp -f $PART dist/miner-info-subset-latest/miner-info-subset-latest.json
+  cat $PART | jq -s "{ date: \"$DATE\", miners: map({ key: .miner, value: { sectorSize: .[\"last(sectorSize)\"], peerId: .[\"last(peerId)\"], multiaddrsDecoded: .[\"last(multiaddrsDecoded)\"] } }) | from_entries }" > dist/miner-info-subset-latest/miner-info-subset-latest.json
 fi
 (cd dist/miner-info-subset-latest; head miner-info-subset-latest.json; hub bucket push -y)
 
