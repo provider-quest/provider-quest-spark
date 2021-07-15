bd18e93e37dd20e058477ee43ec636481e50c521
Jim Pick <jim@jimpick.com>
2021-07-15 09:31:23 -0700

    Publish daily dht-addrs counts

--- publish-dht-addrs.sh
+++ publish-dht-addrs.sh
@@ -5,6 +5,7 @@ DATE=$(node -e 'console.log((new Date()).toISOString())')
 
 # Latest DHT addrs
 mkdir -p dist/dht-addrs-latest
+
 if [ -f output/dht_addrs/json_latest_subset/_SUCCESS ] ; then
   PART=$(ls output/dht_addrs/json_latest_subset/part*.json | head -1)
   cat $PART | jq -s "{ \
@@ -26,5 +27,20 @@ if [ -f output/dht_addrs/json_latest_subset/_SUCCESS ] ; then
     }) | from_entries \
   }" > dist/dht-addrs-latest/dht-addrs-latest.json
 fi
+
+# Daily counts
+LASTDAILY=$(ls -d output/dht_addrs/json_counts_daily/date\=* | sort | tail -1)
+echo $LASTDAILY
+DATEDAILY=$(echo $LASTDAILY | sed 's,^.*date=,,')
+echo $DATEDAILY
+cat output/dht_addrs/json_counts_daily/date\=$DATEDAILY/part-*.json |
+  jq -s "{ \
+    date: \"$DATEDAILY\", \
+    miners: map({ \
+      key: .miner, \
+      value: .count \
+    }) | from_entries \
+  }" > dist/dht-addrs-latest/dht-addrs-counts-daily.json
+
 (cd dist/dht-addrs-latest; head dht-addrs-latest.json; hub bucket push -y)
 
