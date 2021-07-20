5b54eca7b0c1e29280a7ebfdb029e125eb7c0368
Jim Pick <jim@jimpick.com>
2021-07-19 19:56:02 -0700

    Improve latest power publish script

--- publish-power-latest.sh
+++ publish-power-latest.sh
@@ -9,32 +9,6 @@ mkdir -p dist/miner-power-daily-average-latest
 if [ -f output/miner_power/json_latest/_SUCCESS ] ; then
   PART=$(ls output/miner_power/json_latest/part*.json | head -1)
 
-  #cat $PART | jq -s ".[] | select(.miner != null) | { \
-  #  date: \"$DATE\", \
-  #  miners: map({ \
-  #    key: .miner, \
-  #    value: { \
-  #      epoch: .[\"last(epoch)\"], \
-  #      timestamp: .[\"last(timestamp)\"], \
-  #      rawBytePower: .[\"last(rawBytePower)\"], \
-  #      qualityAdjPower: .[\"last(qualityAdjPower)\"], \
-  #    } | to_entries | [(.[] | select(.value != null))] | from_entries \
-  #  }) | from_entries \
-  #}" > dist/miner-power-daily-average-latest/miner-power-latest.json
-
-  #cat $PART | jq -s "{ \
-  #  date: \"$DATE\", \
-  #  miners: [.] | select(.miner != null) | .map({ \
-  #    key: .miner, \
-  #    value: true \
-  #  }) \
-  #}" > dist/miner-power-daily-average-latest/miner-power-latest.json
-
-  #cat $PART | jq -s "{ \
-  #  date: \"$DATE\", \
-  #  miners: map(.miner) \
-  #}"
-
   cat $PART | jq -s "{ \
     date: \"$DATE\", \
     miners: [.[] | select(.miner != null)] | map({ \
@@ -50,5 +24,5 @@ if [ -f output/miner_power/json_latest/_SUCCESS ] ; then
 
 fi
 
-#(cd dist/miner-info-subset-latest; head miner-info-subset-latest.json; hub bucket push -y)
+(cd dist/miner-power-daily-average-latest; head miner-power-latest.json; hub bucket push -y)
 
