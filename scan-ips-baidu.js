d1e7cf2215d4fdd9e55d6caadfbb4f56ec4e496d
Jim Pick <jim@jimpick.com>
2021-08-01 19:46:40 -0700

    Script to do geoip lookups on Baidu

---
+++ scan-ips-baidu.js
@@ -0,0 +1,75 @@
+const fs = require('fs')
+const { load } = require('@alex.garcia/observable-prerender')
+const dateFns = require('date-fns')
+const delay = require('delay')
+require('dotenv').config()
+
+fs.mkdirSync('input/ips-baidu', { recursive: true })
+
+async function run () {
+  let jsonFilename
+  const notebook = await load(
+    '@jimpick/miner-report-baidu-ip-geo-lookups',
+    ['ipsBaidu', 'currentEpoch', 'currentEpochDate']
+    // { headless: false }
+  )
+  const currentEpoch = await notebook.value('currentEpoch')
+  const currentEpochDate = await notebook.value('currentEpochDate')
+  console.log('Date:', currentEpochDate)
+  let count = 0
+  while (true) {
+    const ipsBaidu = await notebook.value('ipsBaidu')
+    if (ipsBaidu.state === 'paused') {
+      await notebook.redefine('geoIpBaiduKey', process.env.GEOIP_BAIDU_KEY)
+      await notebook.redefine('geoIpBaiduSecret', process.env.GEOIP_BAIDU_SECRET)
+      await notebook.redefine('maxLookups', 50)
+      await notebook.redefine('maxElapsed', 1 * 60 * 1000)
+      await notebook.redefine('start', 1)
+      await delay(1000)
+      continue
+    }
+    if (count++ % 100 === 0) {
+      console.log(
+        `ips-baidu${process.argv[2] ? ' ' + process.argv[2] : ''} => State:`,
+        ipsBaidu.state,
+        ipsBaidu.elapsed ? `Elapsed: ${dateFns.formatDistance(ipsBaidu.elapsed * 1000, 0)}` : '',
+        'Scanned:',
+        ipsBaidu.scannedIps + '/' + ipsBaidu.totalIps,
+        'Records:',
+        ipsBaidu.recordsLength,
+        'Errors:',
+        ipsBaidu.errors
+      )
+    }
+    if (ipsBaidu.state === 'done') {
+      count = ipsBaidu.records.length
+      jsonFilename = `ips-baidu-${currentEpoch}.json`
+      const jsonFile = fs.createWriteStream(`tmp/${jsonFilename}`)
+      for (const record of ipsBaidu.records) {
+        await jsonFile.write(
+          JSON.stringify({
+            timestamp: currentEpochDate,
+            epoch: currentEpoch,
+            ...record
+          }) + '\n'
+        )
+      }
+      jsonFile.on('finish', () => {
+        fs.rename(`tmp/${jsonFilename}`, `input/ips-baidu/${jsonFilename}`, err => {
+          if (err) {
+            console.error('Error', err)
+            process.exit(1)
+          }
+        })
+      })
+      jsonFile.end()
+      break
+    }
+  }
+  console.log('Filename:', jsonFilename)
+  console.log('Epoch:', currentEpoch)
+  console.log('Date:', currentEpochDate)
+  console.log('Records:', count)
+  await notebook.browser.close()
+}
+run()
