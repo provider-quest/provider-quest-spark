e655429dd447fa2cd33f75609ca62d950e060bee
Jim Pick <jim@jimpick.com>
2021-08-15 11:33:38 -0700

    Fix .gitignore

---
+++ web3-storage-archive/archive.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+. ../.env
+
+#DIR=ips-baidu
+#echo node put-files.js --token=\$WEB3_STORAGE_API_TOKEN ../estuary-archive/$DIR
+#node put-files.js --token=$WEB3_STORAGE_API_TOKEN ../estuary-archive/$DIR
+
+# too many open files
+#echo node put-files.js --token=\$WEB3_STORAGE_API_TOKEN ../estuary-archive
+#node put-files.js --token=$WEB3_STORAGE_API_TOKEN ../estuary-archive
+
+for d in ls ../estuary-archive/*; do
+  if [ -d "$d" ]; then
+    echo ">>" node put-files.js --token=\$WEB3_STORAGE_API_TOKEN $d
+    node put-files.js --token=$WEB3_STORAGE_API_TOKEN $d
+  fi
+done
