bd6a50f1533f362f2884f2f514ac317a7ffcb357
Jim Pick <jim@jimpick.com>
2021-08-18 10:05:27 -0700

    Scan region hierarchy

---
+++ clean-by-miner-region.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+rm -rf output/miner_power/by_miner_region/ checkpoint/miner_power/by_miner_region/ output/deals/by_miner_region/ checkpoint/deals/by_miner_region/
--- scan-miner-regions-locations.js
+++ scan-miner-regions-locations.js
@@ -8,12 +8,13 @@ fs.mkdirSync('input/miner-locations', { recursive: true })
 async function run () {
   const notebook = await load(
     '@jimpick/miner-report-miner-to-region-mapper',
-    ['minerRegionsTable', 'minerLocationsTable', 'currentEpoch']
+    ['minerRegionsTable', 'minerLocationsTable', 'currentEpoch', 'regionHierarchy']
     // { headless: false }
   )
   const epoch = await notebook.value('currentEpoch')
   const minerRegions = await notebook.value('minerRegionsTable')
   const minerLocations = await notebook.value('minerLocationsTable')
+  const regionHierarchy = await notebook.value('regionHierarchy')
   await notebook.browser.close()
 
   async function writeMinerRegions () {
@@ -68,8 +69,29 @@ async function run () {
     }
   }
 
+  async function writeRegionHierarchy () {
+    fs.mkdirSync(`input/region-hierarchy/${epoch}`, { recursive: true })
+    const jsonFilename = `region-hierarchy-${epoch}.json`
+    const dest =`input/region-hierarchy/${epoch}/${jsonFilename}` 
+    if (fs.existsSync(dest)) {
+      console.log(`File already exists, skipping. ${jsonFilename}`)
+    } else {
+      fs.writeFileSync(`tmp/${jsonFilename}`,
+                       JSON.stringify(regionHierarchy, null, 2))
+      fs.rename(`tmp/${jsonFilename}`, dest, err => {
+        if (err) {
+          console.error('Error', err)
+          process.exit(1)
+        }
+      })
+      console.log('Filename:', jsonFilename)
+      console.log('Epoch:', epoch)
+    }
+  }
+
   await writeMinerRegions()
   await writeMinerLocations()
+  await writeRegionHierarchy()
 
   await notebook.browser.close()
 }
