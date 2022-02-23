21fb97ca259dff9f317441e79f74dd4912385f53
Jim Pick <jim@jimpick.com>
2022-02-23 11:38:30 -0800

    Add cowsay (for argo workflow experimenting)

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+docker rm -f pq-spark
 docker run -it --cap-add=SYS_ADMIN --entrypoint /bin/bash --cap-add=SYS_ADMIN --name pq-spark pq-spark
+#docker run -it -u 0 --cap-add=SYS_ADMIN --entrypoint /bin/bash --cap-add=SYS_ADMIN --name pq-spark pq-spark
