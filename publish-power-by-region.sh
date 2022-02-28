df8a485ed865ce71313ce4e91d90dbc4bc2586b8
Jim Pick <jim@jimpick.com>
2022-02-28 19:59:05 +0000

    Publish power by synthetic regions

--- publish-power-by-region.sh
+++ publish-power-by-region.sh
@@ -21,5 +21,20 @@ if [ -f ../work/output/miner_power/by_miner_region/sum_avg_daily/json/_SUCCESS ]
 
 fi
 
-(cd dist/miner-power-daily-average-latest; head miner-power-by-region.json; hub bucket push -y)
+if [ -f ../work/output/miner_power/by_synthetic_region/sum_avg_daily/json/_SUCCESS ] ; then
+  PART=$(ls ../work/output/miner_power/by_synthetic_region/sum_avg_daily/json/part*.json | head -1)
+
+  cat $PART | jq -s "{ \
+    date: \"$DATE\", \
+    rows: .
+  }" > dist/miner-power-daily-average-latest/provider-power-by-synthetic-region.json
+
+fi
+
+(
+  cd dist/miner-power-daily-average-latest
+  head miner-power-by-region.json
+  head provider-power-by-synthetic-region.json
+  hub bucket push -y
+)
 
---
+++ src/synthetic_regions/synthetic_regions.py
@@ -0,0 +1,26 @@
+import os
+from pyspark.sql.functions import lit
+from pyspark.sql.types import StructType
+
+def get_latest(spark, suffix=""):
+
+    inputDir = 'input' + suffix
+
+    candidates = os.listdir(inputDir + '/synthetic-regions')
+    epoch = sorted([int(n) for n in candidates])[-1]
+
+    schemaSyntheticRegions = StructType() \
+        .add("provider", "string") \
+        .add("region", "string") \
+        .add("numRegions", "short")
+
+    syntheticRegions = spark \
+        .read \
+        .schema(schemaSyntheticRegions) \
+        .json(f"{inputDir}/synthetic-regions/{epoch}/synthetic-provider-regions-{epoch}.json")
+
+    syntheticRegions = syntheticRegions.withColumn("syntheticRegionEpoch", lit(epoch))
+
+    #minerRegions.show()
+
+    return syntheticRegions
