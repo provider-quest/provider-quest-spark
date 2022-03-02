ccfd9d8a44345f4ba1ece1334ff612ed427178cf
Jim Pick <jim@jimpick.com>
2022-03-02 04:40:34 +0000

    Publish synthetic regions and locations

--- publish-regions-locations.sh
+++ publish-regions-locations.sh
@@ -95,7 +95,55 @@ if [ -f input/country-state-province-hierarchy/$LAST_CSP_HIERARCHY/country-state
   }" > dist/geoip-lookups/country-state-province-hierarchy-latest.json
 fi
 
+# Latest synthetic-regions
 
+LAST_SYNTHETIC_REGIONS=$(cd input/synthetic-regions; ls | sort -n | tail -1)
+#echo $LAST_SYNTHETIC_REGIONS
+
+if [ -f input/synthetic-regions/$LAST_SYNTHETIC_REGIONS/synthetic-provider-regions-*.json ] ; then
+  JSON=input/synthetic-regions/$LAST_SYNTHETIC_REGIONS/synthetic-provider-regions-*.json
+  cat $JSON | jq -s "{ \
+    date: \"$DATE\", \
+    epoch: $LAST_SYNTHETIC_REGIONS,
+    regions: . \
+  }" > dist/geoip-lookups/synthetic-regions-latest.json
+fi
+
+LAST_SYNTHETIC_CSP_REGIONS=$(cd input/synthetic-regions; ls | sort -n | tail -1)
+#echo $LAST_SYNTHETIC_CSP_REGIONS
+
+if [ -f input/synthetic-regions/$LAST_SYNTHETIC_CSP_REGIONS/synthetic-provider-country-state-province-*.json ] ; then
+  JSON=input/synthetic-regions/$LAST_SYNTHETIC_CSP_REGIONS/synthetic-provider-country-state-province-*.json
+  cat $JSON | jq -s "{ \
+    date: \"$DATE\", \
+    epoch: $LAST_SYNTHETIC_CSP_REGIONS,
+    regions: . \
+  }" > dist/geoip-lookups/synthetic-country-state-province-latest.json
+fi
+
+LAST_SYNTHETIC_LOCATIONS=$(cd input/synthetic-locations; ls | sort -n | tail -1)
+#echo $LAST_SYNTHETIC_LOCATIONS
+
+if [ -f input/synthetic-locations/$LAST_SYNTHETIC_LOCATIONS/synthetic-provider-locations-*.json ] ; then
+  JSON=input/synthetic-locations/$LAST_SYNTHETIC_LOCATIONS/synthetic-provider-locations-*.json
+  cat $JSON | jq -s "{ \
+    date: \"$DATE\", \
+    epoch: $LAST_SYNTHETIC_LOCATIONS,
+    providerLocations: . \
+  }" > dist/geoip-lookups/synthetic-locations-latest.json
+fi
+
+LAST_SYNTHETIC_CSP_LOCATIONS=$(cd input/synthetic-locations; ls | sort -n | tail -1)
+#echo $LAST_SYNTHETIC_CSP_LOCATIONS
+
+if [ -f input/synthetic-locations/$LAST_SYNTHETIC_CSP_LOCATIONS/synthetic-provider-country-state-province-locations-*.json ] ; then
+  JSON=input/synthetic-locations/$LAST_SYNTHETIC_CSP_LOCATIONS/synthetic-provider-country-state-province-locations-*.json
+  cat $JSON | jq -s "{ \
+    date: \"$DATE\", \
+    epoch: $LAST_SYNTHETIC_CSP_LOCATIONS,
+    providerLocations: . \
+  }" > dist/geoip-lookups/synthetic-country-state-province-locations-latest.json
+fi
 
 (
   cd dist/geoip-lookups;
@@ -111,5 +159,13 @@ fi
   head provider-country-state-province-locations-latest.json
   echo "country-state-province-hierarchy-latest.json:"
   head country-state-province-hierarchy-latest.json
+  echo "synthetic-regions-latest.json:"
+  head synthetic-regions-latest.json
+  echo "synthetic-country-state-province-latest.json:"
+  head synthetic-country-state-province-latest.json
+  echo "synthetic-locations-latest.json:"
+  head synthetic-locations-latest.json
+  echo "synthetic-country-state-province-locations-latest.json:"
+  head synthetic-country-state-province-locations-latest.json
   hub bucket push -y
 )
