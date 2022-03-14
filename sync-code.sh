1c3f7b8e16f6f139a8e08de13f344b8f2cfcf0d4
Jim Pick <jim@jimpick.com>
2022-03-13 18:52:04 -0700

    Start new publishing script for use from argo workflow

--- sync-code.sh
+++ sync-code.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
-POD_UID=$(kubectl -n argo get pod spark -o json | jq -r .metadata.uid)
-PVC=$(kubectl -n argo get pvc spark -o json | jq -r .spec.volumeName)
-rsync -vaP --exclude node_modules --exclude sync-code.sh root@nuc2-wired:/var/lib/kubelet/pods/$POD_UID/volumes/kubernetes.io~csi/$PVC/mount/provider-quest-spark/* .
+#POD=spark
+#PVC_NAME=spark
+#POD_UID=$(kubectl -n argo get pod $POD -o json | jq -r .metadata.uid)
+#PVC=$(kubectl -n argo get pvc spark -o json | jq -r .spec.volumeName)
+#rsync -vaP --exclude node_modules --exclude sync-code.sh root@nuc2-wired:/var/lib/kubelet/pods/$POD_UID/volumes/kubernetes.io~csi/$PVC/mount/provider-quest-spark/* .
+
+POD=publisher-test-pod
+POD_UID=$(kubectl -n argo get pod $POD -o json | jq -r .metadata.uid)
+rsync -vaP --exclude node_modules --exclude sync-code.sh root@nuc2-wired:/var/lib/kubelet/pods/$POD_UID/volumes/kubernetes.io~empty-dir/work-vol/provider-quest-spark/* .
+
