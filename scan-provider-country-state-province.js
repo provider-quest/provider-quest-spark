9a44aff23fc5f0f8b84d79bd6a3af66296b29898
Jim Pick <jim@jimpick.com>
2022-03-23 18:39:54 -0700

    Move scan for country/state/province regions to k8s

--- scan-provider-country-state-province.js
+++ scan-provider-country-state-province.js
@@ -2,8 +2,11 @@ const fs = require('fs')
 const { formatWithOptions } = require('util')
 const { load } = require('@alex.garcia/observable-prerender')
 
-fs.mkdirSync('input/provider-country-state-province', { recursive: true })
-fs.mkdirSync('input/provider-country-state-province-locations', { recursive: true })
+const workDir = process.env.WORK_DIR || '.'
+const tmpDir = `${workDir}/tmp`
+
+fs.mkdirSync(`${workDir}/input/provider-country-state-province`, { recursive: true })
+fs.mkdirSync(`${workDir}/input/provider-country-state-province-locations`, { recursive: true })
 
 async function run () {
   const notebook = await load(
@@ -18,18 +21,18 @@ async function run () {
   await notebook.browser.close()
 
   async function writeMinerRegions () {
-    fs.mkdirSync(`input/provider-country-state-province/${epoch}`, { recursive: true })
+    fs.mkdirSync(`${workDir}/input/provider-country-state-province/${epoch}`, { recursive: true })
     const jsonFilename = `provider-country-state-province-${epoch}.json`
-    const dest =`input/provider-country-state-province/${epoch}/${jsonFilename}` 
+    const dest =`${workDir}/input/provider-country-state-province/${epoch}/${jsonFilename}` 
     if (fs.existsSync(dest)) {
       console.log(`File already exists, skipping. ${jsonFilename}`)
     } else {
-      const jsonFile = fs.createWriteStream(`tmp/${jsonFilename}`)
+      const jsonFile = fs.createWriteStream(`${tmpDir}/${jsonFilename}`)
       for (const record of minerRegions) {
         await jsonFile.write(JSON.stringify(record) + '\n')
       }
       jsonFile.on('finish', () => {
-        fs.rename(`tmp/${jsonFilename}`, dest, err => {
+        fs.rename(`${tmpDir}/${jsonFilename}`, dest, err => {
           if (err) {
             console.error('Error', err)
             process.exit(1)
@@ -44,18 +47,18 @@ async function run () {
   }
 
   async function writeMinerLocations () {
-    fs.mkdirSync(`input/provider-country-state-province-locations/${epoch}`, { recursive: true })
+    fs.mkdirSync(`${workDir}/input/provider-country-state-province-locations/${epoch}`, { recursive: true })
     const jsonFilename = `provider-country-state-province-locations-${epoch}.json`
-    const dest =`input/provider-country-state-province-locations/${epoch}/${jsonFilename}` 
+    const dest =`${workDir}/input/provider-country-state-province-locations/${epoch}/${jsonFilename}` 
     if (fs.existsSync(dest)) {
       console.log(`File already exists, skipping. ${jsonFilename}`)
     } else {
-      const jsonFile = fs.createWriteStream(`tmp/${jsonFilename}`)
+      const jsonFile = fs.createWriteStream(`${tmpDir}/${jsonFilename}`)
       for (const record of minerLocations) {
         await jsonFile.write(JSON.stringify(record) + '\n')
       }
       jsonFile.on('finish', () => {
-        fs.rename(`tmp/${jsonFilename}`, dest, err => {
+        fs.rename(`${tmpDir}/${jsonFilename}`, dest, err => {
           if (err) {
             console.error('Error', err)
             process.exit(1)
@@ -70,15 +73,15 @@ async function run () {
   }
 
   async function writeRegionHierarchy () {
-    fs.mkdirSync(`input/country-state-province-hierarchy/${epoch}`, { recursive: true })
+    fs.mkdirSync(`${workDir}/input/country-state-province-hierarchy/${epoch}`, { recursive: true })
     const jsonFilename = `country-state-province-hierarchy-${epoch}.json`
-    const dest =`input/country-state-province-hierarchy/${epoch}/${jsonFilename}` 
+    const dest =`${workDir}/input/country-state-province-hierarchy/${epoch}/${jsonFilename}` 
     if (fs.existsSync(dest)) {
       console.log(`File already exists, skipping. ${jsonFilename}`)
     } else {
-      fs.writeFileSync(`tmp/${jsonFilename}`,
+      fs.writeFileSync(`${tmpDir}/${jsonFilename}`,
                        JSON.stringify(regionHierarchy, null, 2))
-      fs.rename(`tmp/${jsonFilename}`, dest, err => {
+      fs.rename(`${tmpDir}/${jsonFilename}`, dest, err => {
         if (err) {
           console.error('Error', err)
           process.exit(1)
--- scan-regions-locations-worker.sh
+++ scan-regions-locations-worker.sh
@@ -4,5 +4,5 @@ export TZ=UTC
 
 echo '>> Regions and Locations'
 timeout 1m node scan-miner-regions-locations.js
-#timeout 1m node scan-provider-country-state-province.js
+timeout 1m node scan-provider-country-state-province.js
 
