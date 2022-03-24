01f8be304c09969d7fd8aa3fb1a039e1d1b8fefc
Jim Pick <jim@jimpick.com>
2022-03-24 19:27:46 +0000

    Disable old publish power by CSP

--- publish-power-by-country-state-province.sh
+++ publish-power-by-country-state-province.sh
@@ -11,15 +11,15 @@ DATE=$(node -e 'console.log((new Date()).toISOString())')
 # Latest power
 mkdir -p dist/miner-power-daily-average-latest
 
-if [ -f ../work/output/miner_power/by_provider_country_state_province/sum_avg_daily/json/_SUCCESS ] ; then
-  PART=$(ls ../work/output/miner_power/by_provider_country_state_province/sum_avg_daily/json/part*.json | head -1)
-
-  cat $PART | jq -s "{ \
-    date: \"$DATE\", \
-    rows: .
-  }" > tmp/miner-power-by-country-state-province.json
-
-fi
+#if [ -f ../work/output/miner_power/by_provider_country_state_province/sum_avg_daily/json/_SUCCESS ] ; then
+#  PART=$(ls ../work/output/miner_power/by_provider_country_state_province/sum_avg_daily/json/part*.json | head -1)
+#
+#  cat $PART | jq -s "{ \
+#    date: \"$DATE\", \
+#    rows: .
+#  }" > tmp/miner-power-by-country-state-province.json
+#
+#fi
 
 if [ -f ../work/output/miner_power/by_synthetic_csp_region/sum_avg_daily/json/_SUCCESS ] ; then
   PART=$(ls ../work/output/miner_power/by_synthetic_csp_region/sum_avg_daily/json/part*.json | head -1)
@@ -33,9 +33,9 @@ fi
 
 cd dist/miner-power-daily-average-latest
 hub bucket pull -y
-mv ../../tmp/miner-power-by-country-state-province.json .
+#mv ../../tmp/miner-power-by-country-state-province.json .
 mv ../../tmp/provider-power-by-synthetic-csp-region.json .
-head miner-power-by-country-state-province.json
+#head miner-power-by-country-state-province.json
 head provider-power-by-synthetic-csp-region.json
 hub bucket push -y
 
