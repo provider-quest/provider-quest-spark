7cbd7ffc0a08d03b6f901c45950a2a86bc8c92c8
Jim Pick <jim@jimpick.com>
2022-04-05 18:49:32 -0700

    Write json output for synthetic locations to sink

---
+++ .vimrc
@@ -0,0 +1,5 @@
+set tabstop=2
+set shiftwidth=2
+set autoindent
+set smartindent
+set expandtab
--- scan-synthetic-locations.js
+++ scan-synthetic-locations.js
@@ -8,19 +8,19 @@ const workDir = process.env.WORK_DIR || '.'
 const tmpDir = `${workDir}/tmp`
 
 async function run () {
-  let jsonFilename
   const notebook = await load(
     '@jimpick/internal-provider-funding-tree-provider-quest-test-2',
     [
       'minersAndFundersUrl',
-      'cryptoApiPresent',
+      'currentEpoch',
+      'currentEpochDate',
       'funderTreeWithDelegatesProgressWithoutResult'
     ],
     { headless: true }
   )
   // await notebook.redefine('interactiveEpoch', 1451584) // Override
-  // const selectedEpoch = await notebook.value('selectedEpoch')
-  // const selectedDate = await notebook.value('selectedDate')
+  const currentEpoch = await notebook.value('currentEpoch')
+  const currentEpochDate = await notebook.value('currentEpochDate')
   // console.log('Date:', selectedDate)
   // const tipSet = await notebook.value('tipSet')
   /*
@@ -54,23 +54,57 @@ async function run () {
       console.log('Progress', JSON.stringify(progress))
       if (progress.done) {
         console.log('Done')
-        const outputEpoch = 'xxx'
-        const cspRegions = await notebook.value('syntheticProviderCSPRegions')
-        jsonFilename = `synthetic-provider-country-state-province-${outputEpoch}.json`
-        const jsonFile = fs.createWriteStream(`${tmpDir}/${jsonFilename}`)
-        numRecords = cspRegions.length
-        for (const record of cspRegions) {
-          await jsonFile.write(JSON.stringify(record) + '\n')
-        }
-        jsonFile.on('finish', () => {
-          fs.rename(`${tmpDir}/${jsonFilename}`, `${workDir}/input/tmp/${jsonFilename}`, err => {
-            if (err) {
-              console.error('Error', err)
-              process.exit(1)
-            }
+        const outputEpoch = currentEpoch
+
+        await writeJson(
+          'syntheticProviderCSPRegions',
+          'synthetic-regions',
+          'synthetic-provider-country-state-province'
+        )
+        await writeJson(
+          'syntheticProviderRegions',
+          'synthetic-regions',
+          'synthetic-provider-regions'
+        )
+        await writeJson(
+          'syntheticProviderCSPLocations',
+          'synthetic-locations',
+          'synthetic-provider-country-state-province-locations'
+        )
+        await writeJson(
+          'syntheticProviderLocations',
+          'synthetic-locations',
+          'synthetic-provider-locations'
+        )
+
+        async function writeJson (cellName, dirName, filePrefix) {
+          const cspRegions = await notebook.value(cellName)
+          const jsonFilename = `${filePrefix}-${outputEpoch}.json`
+          const outputTmpDir = process.env.SYNTHETIC_LOCATIONS_SINK_DIR ?
+            `${process.env.SYNTHETIC_LOCATIONS_SINK_DIR}/tmp` : tmpDir
+
+          fs.mkdirSync(outputTmpDir, { recursive: true })
+          const jsonFile = fs.createWriteStream(`${outputTmpDir}/${jsonFilename}`)
+          numRecords = cspRegions.length
+          for (const record of cspRegions) {
+            await jsonFile.write(JSON.stringify(record) + '\n')
+          }
+          jsonFile.on('finish', () => {
+            const outputDir = process.env.SYNTHETIC_LOCATIONS_SINK_DIR ?
+              `${process.env.SYNTHETIC_LOCATIONS_SINK_DIR}/${dirName}/${outputEpoch}` :
+              `${workDir}/input/${dirName}/${outputEpoch}`
+            fs.mkdirSync(outputDir, { recursive: true })
+            fs.rename(`${outputTmpDir}/${jsonFilename}`, `${outputDir}/${jsonFilename}`, err => {
+              if (err) {
+                console.error('Error', err)
+                process.exit(1)
+              }
+            })
+            console.log(`Wrote ${jsonFilename}`)
           })
-        })
-        jsonFile.end()
+          jsonFile.end()
+        }
+
         break
       }
     } catch (e) {
