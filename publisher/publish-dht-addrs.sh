8157a6652fab3e965356f0dd7c087929c2ef2780
Jim Pick <jim@jimpick.com>
2022-04-23 21:55:30 -0700

    Add missing file

---
+++ publisher/publish-dht-addrs.sh
@@ -0,0 +1,95 @@
+#! /bin/bash
+
+set -e
+set +x
+
+TMP=$WORK_DIR/tmp
+mkdir -p $TMP
+
+./setup-textile.sh
+
+TARGET=$WORK_DIR/dist/dht-addrs-latest
+if [ ! -d $TARGET ]; then
+        mkdir -p $TARGET
+        cd $TARGET
+        hub bucket init \
+                --thread $TEXTILE_BUCKET_THREAD \
+                --key $BUCKET_DHT_ADDRS_LATEST_KEY
+fi
+
+IFS="$(printf '\n\t')"
+DATE=$(node -e 'console.log((new Date()).toISOString())')
+
+# Latest DHT addrs
+if [ -f $OUTPUT_DHT_ADDRS_DIR/json_latest_subset/_SUCCESS ] ; then
+  PART=$(ls $OUTPUT_DHT_ADDRS_DIR/json_latest_subset/part*.json | head -1)
+  cat $PART | jq -s "{ \
+    date: \"$DATE\", \
+    miners: map({ \
+      key: .miner, value: { \
+        epoch: .[\"last(epoch)\"], \
+        timestamp: .[\"last(timestamp)\"], \
+        collectedFrom: .[\"last(collectedFrom)\"], \
+        peerId: .[\"last(peerId)\"], \
+        multiaddrs: .[\"last(multiaddrs)\"], \
+        dnsLookups: \
+          (if .[\"last(dnsLookups)\"] then \
+            .[\"last(dnsLookups)\"] | to_entries | map(.value = [.value | map(fromjson)][0]) | from_entries \
+          else \
+            null \
+          end) \
+      } | to_entries | [(.[] | select(.value != null))] | from_entries \
+    }) | from_entries \
+  }" > $TMP/dht-addrs-latest.json
+fi
+
+# Daily counts
+LASTDAILY=$(ls -d $OUTPUT_DHT_ADDRS_DIR/json_counts_daily/date\=* | sort | tail -1)
+echo $LASTDAILY
+DATEDAILY=$(echo $LASTDAILY | sed 's,^.*date=,,')
+echo $DATEDAILY
+cat $OUTPUT_DHT_ADDRS_DIR/json_counts_daily/date\=$DATEDAILY/part-*.json |
+  jq -s "{ \
+    date: \"$DATEDAILY\", \
+    miners: map({ \
+      key: .miner, \
+      value: .count \
+    }) | from_entries \
+  }" > $TMP/dht-addrs-counts-daily.json
+
+# Multiday counts
+LASTMULTIDAY=$(ls -d $OUTPUT_DHT_ADDRS_DIR/json_counts_multiday/window\=* | sort | tail -1)
+echo $LASTMULTIDAY
+DATEMULTIDAY=$(echo $LASTMULTIDAY | sed 's,^.*window=%7B\([^ ]*\).*,\1,')
+echo $DATEMULTIDAY
+cat $LASTMULTIDAY/part-*.json |
+  jq -s "{ \
+    date: \"$DATEMULTIDAY\", \
+    intervalDays: 7, \
+    miners: map({ \
+      key: .miner, \
+      value: .count \
+    }) | from_entries \
+  }" > $TMP/dht-addrs-counts-multiday.json
+
+(
+  set -e
+
+  cd $TARGET
+  hub bucket pull
+
+  mv $TMP/dht-addrs-latest.json .
+  echo "dht-addrs-latest.json:"
+  head dht-addrs-latest.json
+
+  mv $TMP/dht-addrs-counts-daily.json .
+  echo "dht-addrs-counts-daily.json:"
+  head dht-addrs-counts-daily.json
+
+  mv $TMP/dht-addrs-counts-multiday.json .
+  echo "dht-addrs-counts-multiday.json:"
+  head dht-addrs-counts-multiday.json
+
+  hub bucket push -y
+)
+
