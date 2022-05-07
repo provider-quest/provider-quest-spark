0212c99e8ff1e2ad246832482b8475f7b02f2458
Jim Pick <jim@jimpick.com>
2022-05-07 20:03:23 +0000

    Remove deals from legacy

--- pyspark_main_legacy.py
+++ pyspark_main_legacy.py
@@ -12,29 +12,7 @@ if os.path.exists('src.zip'):
 else:
     sys.path.insert(0, './src')
 
-from miner_power import miner_power_source
-from miner_info import miner_info
-from deals import deals_source
-from deals import deals_base
-from deals import deals_by_provider
-from deals import deals_by_client
-from deals import deals_by_pairs
-from deals import deals_sample
-from deals import deals_client_names
-from deals import deals_regions
-from deals import deals_synthetic_regions
-from deals import deals_country_state_province
-from deals import deals_synthetic_csp_regions
-from client_names import client_names
 from asks import asks
-from dht_addrs import dht_addrs
-from multiaddrs_ips import multiaddrs_ips
-from ips_geolite2 import ips_geolite2
-from ips_baidu import ips_baidu
-from miner_regions import miner_regions
-from synthetic_regions import synthetic_regions
-from synthetic_regions import synthetic_csp_regions
-from provider_country_state_province import provider_country_state_province
 
 if __name__ == "__main__":
     spark = SparkSession\
@@ -43,25 +21,6 @@ if __name__ == "__main__":
         .getOrCreate()
 
     suffix = ''
-
-    names = client_names.get(spark, suffix)
-
-    minerRegions = miner_regions.get_latest(spark, suffix)
-    syntheticRegions = synthetic_regions.get_latest(spark, suffix)
-    providerCountryStateProvinces = provider_country_state_province.get_latest(spark, suffix)
-    syntheticCSPRegions = synthetic_csp_regions.get_latest(spark, suffix)
-
-    deals = deals_source.get(spark, suffix)
-    deals_base.process(deals, suffix)
-    deals_by_provider.process(deals, suffix)
-    deals_by_client.process(deals, suffix)
-    deals_by_pairs.process(deals, suffix)
-    deals_sample.process(deals, suffix)
-    deals_client_names.process(deals, names, suffix)
-    deals_regions.process(deals, minerRegions, suffix)
-    deals_synthetic_regions.process(deals, syntheticRegions, suffix)
-    deals_country_state_province.process(deals, providerCountryStateProvinces, suffix)
-    deals_synthetic_csp_regions.process(deals, syntheticCSPRegions, suffix)
 
     asks.process_asks(spark, suffix)
 
