4baedc63ace296e9fa4e29613748cb539f9a8787
Jim Pick <jim@jimpick.com>
2022-05-07 20:05:20 +0000

    Stop scanning deals on legacy

--- scan-worker.sh
+++ scan-worker.sh
@@ -8,69 +8,9 @@ while true; do
   timeout 4m node scan-asks.js --no-recents
 
   echo
-  echo 'Sleeping 60s' $(TZ=America/Vancouver date)
+  echo 'Sleeping 60m' $(TZ=America/Vancouver date)
   echo
-  sleep 60
-
-  echo '>> Deals (1 of 3)'
-  timeout 30m node scan-deals.js
-  echo '>> (finished) Deals (1 of 3)'
-
-  echo
-  echo 'Sleeping 60s' $(TZ=America/Vancouver date)
-  echo
-  sleep 60
-
-  echo '>> Publishing deals'
-  ./publish-deals.sh
-
-  echo
-  echo 'Sleeping 15s' $(TZ=America/Vancouver date)
-  echo
-  sleep 15
-
-  echo '>> Deals (2 of 3)'
-  timeout 30m node scan-deals.js
-  echo '>> (finished) Deals (2 of 3)'
-
-  echo
-  echo 'Sleeping 60s' $(TZ=America/Vancouver date)
-  echo
-  sleep 60
-
-  echo '>> Publishing deals'
-  ./publish-deals.sh
-
-  echo
-  echo 'Sleeping 15s' $(TZ=America/Vancouver date)
-  echo
-  sleep 15
-
-  echo '>> Deals (3 of 3)'
-  timeout 30m node scan-deals.js
-  echo '>> (finished) Deals (3 of 3)'
-
-  echo
-  echo 'Sleeping 30m' $(TZ=America/Vancouver date)
-  echo
-  sleep $((20 * 60))
-
-  echo '>> Publishing everything'
-  ./publish.sh
-
-  echo
-  echo 'Sleeping 15s' $(TZ=America/Vancouver date)
-  echo
-  sleep 15
-
-  echo '>> Regions and Locations'
-  timeout 1m node scan-miner-regions-locations.js
-  timeout 1m node scan-provider-country-state-province.js
-
-  echo
-  echo 'Sleeping 5m' $(TZ=America/Vancouver date)
-  echo
-  sleep $((5 * 60))
+  sleep $((60 * 60))
 
 
 done
